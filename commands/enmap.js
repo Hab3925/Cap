78f07eedf8521225d100cd29ae7b235c84ba5583
Hab3925 <abel.horneland@gmail.com>
2020-10-12 12:12:12 +0200

    Added more options to enmap command

--- commands/enmap.js
+++ commands/enmap.js
@@ -2,6 +2,16 @@ module.exports.run = async (client, message, args) => {
     if (client[args[0]] instanceof Map) {
         if (args[1] == "delete") {
             client[args[0]].deleteAll()
+            message.channel.send(`Deleted content of ${args[0]}`)
+        } else if (args[1] == "set") {
+            if (!args[2] && args[3]) return message.channel.send("You need to add the content you want to set")
+            client[args[0]].set(args[2], args[3])
+            message.channel.send(`Set ${args[2]} = ${args[3]} in ${args[0]}`)
+        } else if (args[0] == "get") {
+            if (!args[2]) return message.channel.send("You have to say what variable you want to `get`")
+            message.channel.send(client[args[0]].get(args[2]))
+        } else {
+            message.channel.send("Invalid operation")
         }
     } else {
         message.channel.send(`Thats not an enmap.`)
