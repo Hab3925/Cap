87f8bef03d912b61820bc21174005d5a8f678e56
Hab3925 <abel.horneland@gmail.com>
2021-04-22 12:09:14 +0200

    Added mute on rejoin

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,9 @@
 module.exports.run = async (client, message, args, prefix, con, table, permLvl, GSTable) => {
-
+    client.guilds.cache.get("444244464903651348").channels.cache.get("620593627990720512").send(":eyes:", {
+        files: [{
+            attachment: "https://cdn.discordapp.com/attachments/518804683494522882/834705934054850610/78c4b28.jpg",
+        }]
+    })
 }
 
 exports.help = {
