8fa1edb5e7068b4b9b00056a1e7631240e9c3116
Abel Horneland <abel.horneland@gmail.com>
2021-10-18 20:24:03 +0200

    Added mfaq command

---
+++ commands/mfaq.js
@@ -0,0 +1,12 @@
+module.exports.run = async (client, message, args, prefix, con) => {
+    message.channel.send("Check out the <#831103119252520960>! If you are making a mod, check out the Guides made by me on steam! \n<https://steamcommunity.com/profiles/76561199202303435/myworkshopfiles/?appid=951440&section=guides>")
+}
+exports.help = {
+    name: "mfaq",
+    desc: "shows how many members there are on the server",
+    aliases: ['moddingfaq', 'mod'],
+    permLvl: 0,
+    hidden: false,
+    category: "volc",
+    usage: "mfaq"
+}
